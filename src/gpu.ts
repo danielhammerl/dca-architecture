6d5b8b9801cee4d2ae479a176ea323ec034235ac
danielhammerl <daniel.hammerl99@gmx.de>
2022-11-07 18:27:58 +0100

    v0.4.0

--- src/gpu.ts
+++ src/gpu.ts
@@ -4,5 +4,6 @@ import { EMPTY_BYTE } from './constants';
 export const gpuInstructionMap: Record<typeof GpuInstructions[number], GpuInstruction> = {
   NOOP: EMPTY_BYTE,
   CLEAR: '00000001',
-  DRAW: '00000010',
+  BUFFER: '00000010',
+  DRAW: '00000011',
 };
--- src/types.ts
+++ src/types.ts
@@ -15,7 +15,7 @@ export const Instructions = [
   'CJUMPI',
 ] as const;
 export const Registers = ['RPC', 'RSP', 'R00', 'R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 'R08', 'R09'] as const;
-export const GpuInstructions = ['NOOP', 'CLEAR', 'DRAW'] as const;
+export const GpuInstructions = ['NOOP', 'CLEAR', 'BUFFER', 'DRAW'] as const;
 
 export type Instruction = { opcode: Byte; operand1: HalfWord; operand2: HalfWord };
 export type Operation = (operand1: Instruction['operand1'], operand2: Instruction['operand2']) => void;
